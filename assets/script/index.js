9cf61b380a25d5cd5344c3677caf7f1b4746f020
Noah Miller <Noseby@live.ca>
2023-05-04 12:16:14 -0500

    posters added

--- assets/script/index.js
+++ assets/script/index.js
@@ -11,10 +11,11 @@
 
 import { select, print } from './utils.js';
 
+
+
 const url = './assets/script/cities.json';
 const url2 = './assets/script/movies.json';
-const list = select('.one');
-const section = select('.two');
+const list = select('section');
 
 function capitalize(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
@@ -74,10 +75,88 @@ async function getMovies() {
 
         const data = await response.json();
         print(data.movies);
+       document.querySelector('section').innerHTML += `
+            <div>
+                <img
+                    class="poster" src="${data.movies[0].photo}"
+                    alt="${(data.movies[0].name)}>
+            </div>
+        ` 
     }   catch(error) {
         print(error.message)
     }
 }
+
+function showMovies() {
+    fetch(url2)
+        .then(response => response.json())
+        .then(data => {
+           document.querySelector('section').innerHTML += `
+           <div class='box'>
+                <img class="img" src=${data.movies[0].image}>
+                <h3>${data.movies[0].name}</h3>
+            </div>
+            <div class='box'>
+                <img class="img" src=${data.movies[1].image}>
+                <h3>${data.movies[1].name}</h3>
+            </div>
+            <div class='box'>
+                <img class="img" src=${data.movies[2].image}>
+                <h3>${data.movies[2].name}</h3>
+            </div>
+            <div class='box'>
+                <img class="img" src=${data.movies[3].image}>
+                <h3>${data.movies[3].name}</h3>
+            </div>
+            <div class='box'>
+                <img class="img" src=${data.movies[4].image}>
+                <h3>${data.movies[4].name}</h3>
+            </div>
+            <div class='box'>
+                <img class="img" src=${data.movies[5].image}>
+                <h3>${data.movies[5].name}</h3>
+            </div>
+            <div class='box'>
+                <img class="img" src=${data.movies[6].image}>
+                <h3>${data.movies[6].name}</h3>
+            </div>
+            <div class='box'>
+                <img class="img" src=${data.movies[7].image}>
+                <h3>${data.movies[7].name}</h3>
+            </div>
+            <div class='box'>
+                <img class="img" src=${data.movies[8].image}>
+                <h3>${data.movies[8].name}</h3>
+            </div>
+            <div class='box'>
+                <img class="img" src=${data.movies[9].image}>
+                <h3>${data.movies[9].name}</h3>
+            </div>
+           `
+        })
+}
+
+showMovies();
+
+/*fetch(url2)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(movies){
+        let placeholder =document.querySelector('section');
+        let output = "";
+        for(let data of movies){
+            out +=
+            `
+                <div class="box">
+                <img src=${data.image}>
+                <h3>${data.name}</h3>
+                </div>
+            `
+        }
+        placeholder.innerHTML = out;
+    })8?*/
+
 
 getCities();
 getMovies();
